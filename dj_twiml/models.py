4a077dd9ff1853f55bd44da0763301b0fa2c1b70
Paul Hallett <hello@phalt.co>
2014-06-16 15:18:58 +0100

    Removed twimlmanager
    Removed twimlmanager in favour of object-scoped functions

--- dj_twiml/models.py
+++ dj_twiml/models.py
@@ -4,25 +4,8 @@ from twilio.twiml import TwimlException
 from xml.etree import ElementTree as ET
 
 
-class TwimlManager(models.Manager):
-
-    def xml_view(self):
-        ''' Return the xml-ified version of the content.
-
-            An XML validation is also performed and will return an
-            XML error if the content is not formatted correctly.
-        '''
-        try:
-            tt = ET.fromstring(self.content)
-        except:
-            raise TwimlException
-        return '<?xml version="1.0" encoding="UTF-8"?>' + self.content
-
-
-
 class Twiml(models.Model):
 
-    objects = TwimlManager()
 
     def __unicode__(self):
         return self.name
@@ -33,3 +16,14 @@ class Twiml(models.Model):
 
     content = models.TextField(max_length=300)
 
+    def xml_view(self):
+            ''' Return the xml-ified version of the content.
+
+                An XML validation is also performed and will return an
+                XML error if the content is not formatted correctly.
+            '''
+            try:
+                tt = ET.fromstring(self.content)
+            except:
+                raise TwimlException
+            return '<?xml version="1.0" encoding="UTF-8"?>' + self.content
