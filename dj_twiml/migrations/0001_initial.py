48f13e27a71c05b22b0d762b3d2f56cc17c33f1b
Paul Hallett <hello@phalt.co>
2014-07-23 09:20:52 +0200

    Django 1.7 migrations file

---
+++ dj_twiml/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Twiml',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=100, blank=True)),
+                ('content', models.TextField(max_length=300)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
